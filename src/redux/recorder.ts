4052e7f5610fa1a09d69717e0fb163bfcefc507e
Gamintor <relkovic10@gmail.com>
2022-03-28 12:51:14 +0200

    progress update

---
+++ src/redux/recorder.ts
@@ -0,0 +1,39 @@
+import { Action } from 'redux';
+import { RootState } from './store';
+
+interface RecorderState {
+	dateStart: string;
+}
+
+const START = 'recorder/start';
+const STOP = 'recorder/stop';
+
+type StartAction = Action<typeof START>;
+type StopAction = Action<typeof STOP>;
+
+export const start = (): StartAction => {
+	return { type: START };
+};
+
+export const stop = (): StopAction => {
+	return { type: STOP };
+};
+
+export const selectDateStart = (rootState: RootState) => rootState.recorder.dateStart;
+
+const initialState: RecorderState = {
+	dateStart: '',
+};
+
+const recorderReducer = (state: RecorderState = initialState, action: StartAction | StopAction) => {
+	switch (action.type) {
+		case START:
+			return { ...state, dateStart: new Date().toISOString() };
+		case STOP:
+			return { ...state, dateStart: '' };
+		default:
+			return state;
+	}
+};
+
+export default recorderReducer;
